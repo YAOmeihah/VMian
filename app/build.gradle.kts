9dad51ff726979e27b78ec1bf6600c0b38132b7c
YAOmeihah <741937692@qq.com>
2026-04-24 02:27:52 +0800

    chore: bump version to 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,8 +49,8 @@ android {
         applicationId = "com.java.vmian"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1.0"
 
         buildConfigField("String", "GITHUB_REPO_OWNER", "\"YAOmeihah\"")
         buildConfigField("String", "GITHUB_REPO_NAME", "\"VMian\"")
